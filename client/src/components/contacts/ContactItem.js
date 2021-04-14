6d494e12e1470e74a0579e4e09d07065e2356901
ParthPatel133 <prpatel133@gmail.com>
2021-04-14 19:05:55 +0530

    React Setup and context setup

---
+++ client/src/components/contacts/ContactItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ContactItem = ({ contact }) => {
+  const { name, email, id, phone, type } = contact;
+
+  return (
+    <div className="card bg-light">
+      <h3 className="text-primary text-left">
+        {name}
+        <span
+          className={`badge ${
+            type === "professional" ? "badge-success" : "badge-primary"
+          }`}
+        ></span>
+      </h3>
+    </div>
+  );
+};
+
+export default ContactItem;
