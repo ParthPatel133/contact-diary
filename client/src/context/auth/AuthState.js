243683707a3959d85470141ff4e33e49c62f5ab3
ParthPatel133 <prpatel133@gmail.com>
2021-04-15 19:35:41 +0530

    user Registration

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,6 +1,8 @@
 import React, { useReducer } from "react";
+import axios from "axios";
 import AuthContext from "./authContext";
 import authReducer from "./authReducer";
+import { CLEAR_ERRORS, REGISTER_FAIL, REGISTER_SUCCESS } from "../types";
 
 const AuthState = (props) => {
   const initialState = {
@@ -16,12 +18,35 @@ const AuthState = (props) => {
   //load user
 
   //register user
+  const register = async (formData) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.post("/api/users", formData, config);
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: error.response.data.msg,
+      });
+    }
+  };
 
   //login user
 
   //logout user
 
   //clear errors
+  const clearErrors = () => {
+    dispatch({ type: CLEAR_ERRORS });
+  };
 
   return (
     <AuthContext.Provider
@@ -31,6 +56,8 @@ const AuthState = (props) => {
         isAuthnticated: state.isAuthnticated,
         loading: state.loading,
         error: state.error,
+        register,
+        clearErrors,
       }}
     >
       {props.children}
--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -1,5 +1,30 @@
+import { CLEAR_ERRORS, REGISTER_FAIL, REGISTER_SUCCESS } from "../types";
+
 const authReducer = (state, action) => {
   switch (action.type) {
+    case REGISTER_SUCCESS:
+      localStorage.setItem("token", action.payload.token);
+      return {
+        ...state,
+        ...action.payload,
+        isAuthnticated: true,
+        loading: false,
+      };
+    case REGISTER_FAIL:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        isAuthnticated: false,
+        loading: false,
+        user: null,
+        error: action.payload,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
     default:
       return state;
   }
